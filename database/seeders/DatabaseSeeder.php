061ddac2b7db4701974df1c8dad5b94a4cba8257
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-26 12:35:12 -0300

    001(Criacao_Projeto)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,24 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        for ($id = 0; $id <= 3; $id++) { 
+            DB::table('users')->insert([
+                'name' => 'User' . $id,
+                'email' => 'user_' . $id . '@gmail.com',
+                'password' => bcrypt('user' . $id),
+                'email_verified_at' => now(),
+                'created_at' => now()
+            ]);
+        }
     }
 }
