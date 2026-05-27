2b9c002fece1c817c341153f1421c86662a3ff3e
Luciano Eduardo <luciano123ab@gmail.com>
2026-05-27 11:19:20 -0300

    011(PrepararCodigo_Subscricao)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -50,9 +50,19 @@ class MainController extends Controller
             return redirect()->route("plans");
         }
 
-        $data = explode("|", $plan);
+        $plan = explode("|", $plan);
+        $product_id = $plan[0];
+        $price_id = $plan[1];
 
-        echo "Product ID: " . $data[0] . "<br>";
-        echo "Price ID: " . $data[1] . "<br>";
+        return auth()->user()
+            ->newSubscription($product_id, $price_id)
+            ->checkout([
+                "success_url" => route("subscription.success"),
+                "cancel_url" => route("plans")
+            ]);
+    }
+
+    public function subscriptionSuccess() {
+        echo "Subscrição realizada com sucesso!";
     }
 }
